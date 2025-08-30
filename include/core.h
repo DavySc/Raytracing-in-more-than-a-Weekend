4415f560b8165a3d96cc093a9b9f9e2699fe35cb
DavySc <d.vdschoot@gmail.com>
2025-08-30 14:51:51 +0200

    Type modifications part 1

---
+++ include/core.h
@@ -0,0 +1,22 @@
+#ifndef CORE_H
+#define CORE_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cmath>
+#include <time.h>
+
+typedef int8_t S8;
+typedef int16_t S16;
+typedef int32_t S32;
+typedef int64_t S64;
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+typedef float F32;
+typedef double F64;
+
+
+#endif // CORE_H
--- include/vec3.h
+++ include/vec3.h
@@ -1,7 +1,7 @@
 #ifndef VEC3_H
 #define VEC3_H
 
-#include <cmath>
+#include "core.h"
 #include <iostream>
 
 class vec3 {
